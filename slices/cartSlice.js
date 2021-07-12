47f10bbcfea06d36fc15d9a041899eaf9c97ac34
tiagomota79 <tiagomot@gmail.com>
2021-07-12 10:32:30 -0400

    Created cart slice and added it to the store

---
+++ slices/cartSlice.js
@@ -0,0 +1,54 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+import PRODUCTS from '../data/dummy-data';
+
+import CartItem from '../model/cart-item';
+
+const initialState = {
+  items: {},
+  totalAmount: 0,
+};
+
+export const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      const product = action.payload;
+      const productTitle = product.title;
+      const productPrice = product.price;
+      const currentCart = state.items;
+
+      let updatedCartItem;
+
+      if (currentCart[product.id]) {
+        updatedCartItem = new CartItem(
+          state.items[product.id].quantity + 1,
+          productPrice,
+          productTitle,
+          state.items[product.id].sum + productPrice
+        );
+      } else {
+        updatedCartItem = new CartItem(
+          1,
+          productPrice,
+          productTitle,
+          productPrice
+        );
+      }
+
+      return {
+        ...state,
+        items: { ...state.items, [product.id]: updatedCartItem },
+        totalAmount: state.totalAmount + productPrice,
+      };
+    },
+  },
+});
+
+export const { addToCart } = cartSlice.actions;
+
+export const selectItemInCart = (state) => state.cart.items;
+export const selectTotalAmount = (state) => state.cart.totalAmount;
+
+export default cartSlice.reducer;
