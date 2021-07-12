988e10c4b350c7515308842bb12ab84acde2be0d
tiagomota79 <tiagomot@gmail.com>
2021-07-12 11:55:27 -0400

    Created CustomHeaderButton component

---
+++ components/UI/HeaderButton.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { HeaderButton } from 'react-navigation-header-buttons';
+import { Ionicons } from '@expo/vector-icons';
+
+import colours from '../../constants/colours';
+
+const CustomHeaderButton = (props) => {
+  return (
+    <HeaderButton
+      {...props}
+      IconComponent={Ionicons}
+      iconSize={23}
+      color={Platform.OS === 'ios' ? colours.primary : colours.white}
+    />
+  );
+};
+
+export default CustomHeaderButton;
