055936707f543ff5d333d2ec00059675aec7d748
tiagomota79 <tiagomot@gmail.com>
2021-07-12 15:20:07 -0400

    Added orderReducer to the store

--- store/store.js
+++ store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import productsReducer from '../slices/productsSlice';
 import cartReducer from '../slices/cartSlice';
+import orderReducer from '..//slices/orderSlice';
 
 export const store = configureStore({
   reducer: {
     products: productsReducer,
     cart: cartReducer,
+    order: orderReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
