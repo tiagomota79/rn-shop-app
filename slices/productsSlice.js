4d478a273b19f6de535ddc2060f4fb9aea14e57b
tiagomota79 <tiagomot@gmail.com>
2021-07-14 11:15:41 -0400

    Acced Set and Create products with server connexion

--- slices/productsSlice.js
+++ slices/productsSlice.js
@@ -1,18 +1,74 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 import PRODUCTS from '../data/dummy-data';
 import Product from '../model/product';
+import { firebaseObjectToArray } from '../utils';
+
+const API_URL = 'https://rn-complete-guide-8706e-default-rtdb.firebaseio.com/';
+
+const headers = {
+  'Content-Type': 'application/json',
+};
 
 const initialState = {
   availableProducts: PRODUCTS,
   userProducts: PRODUCTS.filter((product) => product.ownerId === 'u1'),
 };
 
+export const setProducts = createAsyncThunk(
+  'get/products',
+  async (_, { dispatch }) => {
+    try {
+      const response = await axios.get(`${API_URL}products.json`);
+
+      const productsToSet = firebaseObjectToArray(response.data);
+
+      dispatch(setProductsAction(productsToSet));
+    } catch (error) {
+      const errorText = error.response.data;
+      console.error(errorText);
+    }
+  }
+);
+
+export const createProduct = createAsyncThunk(
+  'post/product',
+  async (params, { dispatch, rejectWithValue }) => {
+    const productToCreate = {
+      title: params.title,
+      imageUrl: params.imageUrl,
+      price: params.price,
+      description: params.description,
+    };
+
+    try {
+      const response = await axios.post(
+        `${API_URL}products.json`,
+        productToCreate
+      );
+
+      console.log('Response id', response.data.name);
+    } catch (error) {
+      const errorText = error.response.data;
+      console.error(errorText);
+    }
+  }
+);
+
 export const productsSlice = createSlice({
   name: 'products',
   initialState,
   reducers: {
-    deleteProduct: (state, action) => {
+    setProductsAction: (state, action) => {
+      return {
+        availableProducts: action.payload,
+        userProducts: action.payload.filter(
+          (product) => product.ownerId === 'u1'
+        ),
+      };
+    },
+    deleteProductAction: (state, action) => {
       const productId = action.payload;
 
       return {
@@ -25,7 +81,7 @@ export const productsSlice = createSlice({
         ),
       };
     },
-    createProduct: (state, action) => {
+    createProductAction: (state, action) => {
       const { title, imageUrl, price, description } = action.payload;
 
       const newProduct = new Product(
@@ -43,7 +99,7 @@ export const productsSlice = createSlice({
         userProducts: state.userProducts.concat(newProduct),
       };
     },
-    updateProduct: (state, action) => {
+    updateProductAction: (state, action) => {
       const { id, title, imageUrl, description } = action.payload;
 
       const userProductIndex = state.userProducts.findIndex(
@@ -80,8 +136,12 @@ export const productsSlice = createSlice({
   },
 });
 
-export const { deleteProduct, createProduct, updateProduct } =
-  productsSlice.actions;
+export const {
+  setProductsAction,
+  deleteProductAction,
+  createProductAction,
+  updateProductAction,
+} = productsSlice.actions;
 
 export const selectProducts = (state) => state.products.availableProducts;
 export const selectUserProducts = (state) => state.products.userProducts;
