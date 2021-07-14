0a1743fe001c1a3fc27ab3fa924c8a7a4ce09734
tiagomota79 <tiagomot@gmail.com>
2021-07-14 17:42:42 -0400

    Removed date transformation from Order class declaration

--- model/order.js
+++ model/order.js
@@ -1,15 +1,9 @@
-import { formatDate } from '../utils';
-
 class Order {
   constructor(id, items, totalAmount, date) {
     this.id = id;
     this.items = items;
     this.totalAmount = totalAmount;
     this.date = date;
-  }
-
-  get readableDate() {
-    return formatDate(this.date);
   }
 }
 
