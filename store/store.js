6e01c84da0ae99d082cc09d3e9490e3c6d10362a
tiagomota79 <tiagomot@gmail.com>
2021-07-15 15:17:15 -0400

    Added auth to the store

--- store/store.js
+++ store/store.js
@@ -2,12 +2,14 @@ import { configureStore, combineReducers } from '@reduxjs/toolkit';
 
 import productsReducer from '../slices/productsSlice';
 import cartReducer from '../slices/cartSlice';
-import orderReducer from '..//slices/orderSlice';
+import orderReducer from '../slices/orderSlice';
+import authReducer from '../slices/authSlice';
 
 const rootReducer = combineReducers({
   products: productsReducer,
   cart: cartReducer,
   order: orderReducer,
+  auth: authReducer,
 });
 
 export const store = configureStore({
